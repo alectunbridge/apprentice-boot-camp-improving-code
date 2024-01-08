e770a5dcfaf6169755cb3fe81715582bf551ae4e
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2024-01-08 13:47:36 +0000

    Fix #89 by introducing the intentional error

--- exercises/nodejs/src/game.js
+++ exercises/nodejs/src/game.js
@@ -121,7 +121,7 @@ var Game = function () {
         return true
       }
     } else {
-      console.log('Answer was correct!!!!')
+      console.log('Answer was corrent!!!!')
 
       purses[currentPlayer] += 1
       console.log(players[currentPlayer] + ' now has ' +
